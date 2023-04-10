f93637e9b30e2e76c993d7f8ed91989fa5253c3a
hyunbell <man.of.backend@gmail.com>
2023-04-11 03:36:00 +0900

    sse 연습 완료

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import { Button } from "ui";
 
 export default function Web() {
+  const [messages, setMessages] = useState<string[]>([]);
+  
+  useEffect(() => {
+      let eventSource: EventSource;
+      const fetchSse = async () => {
+        try {
+          eventSource = new EventSource(
+            `http://localhost:3000/sse`
+          );
+
+          /* EVENTSOURCE ONMESSAGE ---------------------------------------------------- */
+          eventSource.onmessage = async (event) => {
+            const res = await event.data;
+            setMessages((prev) => [...prev, res]);
+          };
+
+          /* EVENTSOURCE ONERROR ------------------------------------------------------ */
+          eventSource.onerror = async (event) => {
+              eventSource.close();
+          };
+        } catch (error) {}
+      };
+      fetchSse();
+      return () => eventSource.close();
+    
+  }, []);
+
   return (
     <div>
-      <h1>Web</h1>
+      <ul>
+        {messages.map((message, i) => <li key={i}>{message}</li>)}
+      </ul>
       <Button />
     </div>
   );
